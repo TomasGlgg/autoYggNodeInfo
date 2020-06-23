7a8c45a76b1b9ec9aa509594e83c67c841b62f42
TomasGlgg <globistomas@gmail.com>
2020-06-23 22:54:49 +0300

    added ignoring comments

--- update.py
+++ update.py
@@ -10,6 +10,21 @@ def update_config(data):  # update config
         data['NodeInfo'][name] = command_out
 
 
+def delete_comments(data):
+    data = data.split('\n')
+    out_data = ''
+    for line in data:
+        line = line.strip()
+        if line.count('#') > 0:
+            if line[0] == '#':
+                out_data += '\n'
+            else:
+                out_data += line[:line.index('#')] + '\n'
+        else:
+            out_data += line
+    return out_data
+
+
 def save_config(data, output_config):
     file = open(output_config, 'w')
     json.dump(data, file, sort_keys=True, indent=4)
@@ -21,11 +36,13 @@ if __name__ == '__main__':
     input_config = argv[1]
     output_config = argv[2]
 
-    #  $python3 update.py /etc/yggdrasil.conf /etc/yggdrasil.conf
+    #  $ python3 update.py /etc/yggdrasil.conf /etc/yggdrasil.conf
 
     print('Opening input config...')
-    data = json.load(open(input_config))
+    raw = open(input_config).read()
     print('Input config loaded, updating config...')
+    raw = delete_comments(raw)
+    data = json.loads(raw)
     update_config(data)
     print('Config updated, saving...')
     save_config(data, output_config)
