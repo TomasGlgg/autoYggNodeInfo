fd87683ab45b3d42d62553a5691f2213dc5cad91
TomasGlgg <globistomas@gmail.com>
2020-06-23 20:55:15 +0300

    load.py example update

--- load.py
+++ load.py
@@ -1,9 +1,13 @@
+'''
+Примерный скрипт для вывода средней нагрузки.
+'''
+
 from subprocess import Popen, PIPE
 
 process = Popen('uptime', shell=True, stdout=PIPE)
 out = process.communicate()[0].decode().split()
-a = out[-3][:-1].replace(',', '.')
-b = out[-2][:-1].replace(',', '.')
-c = out[-1][:-1].replace(',', '.')
+avg_1 = out[-3][:-1].replace(',', '.')  # Убираем линюю информацию
+avg_5 = out[-2][:-1].replace(',', '.')  # Оставляем только значения
+avf_15 = out[-1][:-1].replace(',', '.')
 
-print('Load average:', a, b, c)
+print('Load average:', avg_1, avg_5, avf_15)
